6da4c374a45e3cba5a3660006ab7998dbc7f98bf
yonk88 <yonkland1206@gmail.com>
2017-11-23 10:14:48 +0900

    don't pull

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -5,16 +5,19 @@ $(document).ready(function() {
 	//메뉴 클릭 이벤트
 	$("#teamBtn").click(function() {
 		$("#matchList").load("/spomatch/board/matchList.do");
+		$('#catgTab a:first').tab('show');
 		 //alert("teamBtn : ");
 	});
 
 	$("#recuBtn").click(function() {
 		$("#matchList").load("/spomatch/board/recruitList.do");
+		$('#catgTab a:first').tab('show');
 		//alert("recuBtn");
 	});
 
 	$("#leagueBtn").click(function() {
 		//alert("leagueBtn");
+		$('#catgTab a:first').tab('show');
 		$("#matchList").load("/spomatch/board/matchList.do");
 	});
 
